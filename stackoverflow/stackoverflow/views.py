d3686c3591fa4a4c82e737f94806120d8992eba5
Grigoriy <gri003p@mail.ru>
2023-11-14 18:45:28 +0300

    added pagination (complited 2 dz)

--- stackoverflow/stackoverflow/views.py
+++ stackoverflow/stackoverflow/views.py
@@ -4,23 +4,6 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 IS_AUTH = True
 
-POPULAR_TAGS = [
-    {'id': 1, 'name': 'perl'},
-    {'id': 2, 'name': 'python'},
-    {'id': 3, 'name': 'techopark'},
-    {'id': 4, 'name': 'mySQL'},
-    {'id': 5, 'name': 'django'},
-    {'id': 6, 'name': 'Mail.ru'},
-    {'id': 7, 'name': 'Voloshin'},
-    {'id': 8, 'name': 'FireFox'}
-]
-
-BEST_MEMBERS = [
-    {'id': 1, 'name': 'Mr. Freeman'},
-    {'id': 2, 'name': 'V. Pupkin'},
-    {'id': 3, 'name': 'Ivanov'},
-]
-
 ANSWERS = [
             {
             'id': i,
@@ -31,7 +14,7 @@ ANSWERS = [
                 Our asynchronous knowledge management and collaboration offering, Stack Overflow for Teams, is
                 transforming how people work.""",
             'is_correct': True
-        }  for i in range(15)
+        }  for i in range(5)
 ]
 
 TAGS = [
@@ -49,6 +32,7 @@ TAGS = [
     {'id': 12, 'name': 'large-datasets'}
 ]
 
+
 QUESTIONS = [
     {
         'id': i,
@@ -58,23 +42,16 @@ QUESTIONS = [
         'answer_count': 1,
         'like_count': 2
     } for i in range(50)
+] + [
+    {
+        'id': i,
+        'title': f'another question number {i}',
+        'body': f'this is another very very long question... that has {i} id',
+        'tags': ['django', 'FireFox'],
+        'answer_count': 1,
+        'like_count': 2
+    } for i in range(50)
 ]
-
-def tag(request, tag_name):
-    tag_request = None
-    for tag in TAGS:
-        if tag['name'] == tag_name:
-            tag_request = tag
-            break
-
-    tagged_questions = [question for question in QUESTIONS if tag_name in question['tags']]
-    page_items = paginate(tagged_questions, request, per_page=4)
-
-    return render(request, 'tag.html', context={'tag': tag_request,
-                                                 'questions': page_items,
-                                                   'is_auth': True,
-                                                   'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
 
 def paginate(objects, request, per_page=3):
     try:
@@ -93,6 +70,21 @@ def paginate(objects, request, per_page=3):
 
     return page_items
 
+def popular_tags():
+    return TAGS[:8]
+
+def get_tag_by_name(tag_name):
+    return {tag['name']: tag for tag in TAGS}.get(tag_name)
+
+def tag(request, tag_name):
+    tag_request = get_tag_by_name(tag_name)
+    tagged_questions = [question for question in QUESTIONS if tag_name in question['tags']]
+    page_items = paginate(tagged_questions, request, per_page=4)
+
+    return render(request, 'tag.html', context={'tag': tag_request,
+                                                 'questions': page_items,
+                                                   'is_auth': True,
+                                                   'POPULAR_TAGS': popular_tags()})
 
 def questionList(request):
     all_questions = QUESTIONS
@@ -101,44 +93,35 @@ def questionList(request):
     return render(request, 'questionList.html', context={
         'questions': page_items,
         'is_auth': True,
-        'POPULAR_TAGS': POPULAR_TAGS,
-        'BEST_MEMBERS': BEST_MEMBERS
+        'POPULAR_TAGS': popular_tags()
     })
-
-
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
     return render(request, 'question.html',  context={'questions': item,
                                                        'is_auth': False, 
-                                                       'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS,
+                                                       'POPULAR_TAGS': popular_tags(),
                                                         'ANSWERS': ANSWERS})
 
 def ask(request):
     return render(request, 'ask.html', context= {'is_auth': False,
-                                                  'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
+                                                 'POPULAR_TAGS': popular_tags()})
 
 def login(request):
     return render(request, 'login.html', context= {'is_auth': False,
-                                                    'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
+                                                   'POPULAR_TAGS': popular_tags()})
 
 def register(request):
     return render(request, 'register.html', context= {'is_auth': False,
-                                                       'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
+                                                       'POPULAR_TAGS': popular_tags()})
 
 def settings(request):
     return render(request, 'settings.html', context= {'is_auth': True,
-                                                        'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
+                                                      'POPULAR_TAGS': popular_tags()})
 
 def hot(request):
     all_questions = QUESTIONS
     page_items = paginate(all_questions, request, per_page=5)
     return render(request, 'hot.html', context={'questions': all_questions,
                                                  'is_auth': False,
-                                                   'POPULAR_TAGS': POPULAR_TAGS,
-                                                        'BEST_MEMBERS': BEST_MEMBERS})
+                                                   'POPULAR_TAGS': popular_tags()})
